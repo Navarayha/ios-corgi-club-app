e64f5a19e8ec7bf9864dab47f0f148a8c65bcaf0
mi1212 <mc_gorba4ev@mail.ru>
2022-09-23 09:09:26 +0300

    добавлена белая кнопка CCWhiteColor

---
+++ Features/CommonUI/Sources/CCWhiteButton.swift
@@ -0,0 +1,38 @@
+//
+//  CCWhiteButton.swift
+//  CommonUI
+//
+//  Created by Mikhail Chuparnov on 23.09.2022.
+//  Copyright © 2022 Sonomos.com. All rights reserved.
+//
+
+import UIKit
+
+
+class CCWhiteButton: UIButton {
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public init(title: String) {
+        super.init(frame: .zero)
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    private func configure() {
+        self.backgroundColor = .white
+        self.titleLabel?.font = UIFont.systemFont(ofSize: UIFont.buttonFontSize.nextUp)
+        self.setTitleColor(UIColor(named: "color", in: CommonUIResources.bundle, compatibleWith: nil), for: .normal)
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+
+    
