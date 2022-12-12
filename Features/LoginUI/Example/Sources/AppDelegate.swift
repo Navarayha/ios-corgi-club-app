2b51ebb5355766a12b4f30166f2eaa492a682f92
Navarayha <111189397+Navarayha@users.noreply.github.com>
2022-12-12 13:34:18 +0300

    Feature/21 (#23)
    * Added project version, build number. Removed unused files.
    
    * M1 stuff
    
    Co-authored-by: Sergio <afirthes@gmail.com>

--- Features/LoginUI/Example/Sources/AppDelegate.swift
+++ Features/LoginUI/Example/Sources/AppDelegate.swift
@@ -1,11 +1,38 @@
 import UIKit
+import Common
 import LoginUI
 import FirebaseCore
+import FirebaseAuth
+import FirebaseFirestore
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, LoginViewControllerDelegate {
+    
+    func doLogin(mail: String, password: String) {
+        
+        
+        print("hi")
+        
+//        Firestore.firestore().collection("doggies").getDocuments() { (querySnapshot, err) in
+//            if let err = err {
+//                print("Error getting documents: \(err)")
+//            } else {
+//                for document in querySnapshot!.documents {
+//                    print("\(document.documentID) => \(document.data())")
+//                }
+//            }
+//        }
+        
+
+       Firestore.firestore().collection("doggies").addDocument(data: ["name": "hello"])
+
+        
+        
+    }
 
     var window: UIWindow?
+    
+    var loginController: LoginViewController!
 
     func application(
         _ application: UIApplication,
@@ -14,8 +41,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         FirebaseApp.configure()
         
+        Auth.auth().signIn(withEmail: "me@me.me", password: "123456") { user, error in
+          if let error = error, user == nil {
+            print("Login error \(error)")
+          } else {
+            print("Seems everything is fine!")
+          }
+        }
+        
+        loginController = LoginViewController()
+        loginController.delegate = self
+        
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController(rootViewController: LoginViewController())
+        window?.rootViewController = UINavigationController(rootViewController: loginController)
         window?.makeKeyAndVisible()
         
         return true
--- Tuist/Dependencies.swift
+++
@@ -1,9 +0,0 @@
-import ProjectDescription
-
-let dependencies = Dependencies(
-    swiftPackageManager: [
-        .remote(url: "https://github.com/firebase/firebase-ios-sdk", requirement:.upToNextMajor(from:"9.0.0")),
-        .remote(url: "https://github.com/Alamofire/Alamofire.git", requirement:.upToNextMajor(from: "5.6.1"))
-    ],
-    platforms: [.iOS]
-)
